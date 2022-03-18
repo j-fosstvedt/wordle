0a2c1a04cebb20413c952da91c4021793de72803
jorgen-tharaldsen-fosstvedt <j.fosstvedt@gmail.com>
2022-03-18 20:01:59 +0100

    idk

--- wordle.py
+++ wordle.py
@@ -2,7 +2,7 @@ import random
 answersFile = open("C:/Python/wordle/answers.txt", "r")
 
 def startGame():
-    answer = answersFile.(random.randrange(0, 10000), )
+    answer = answersFile.readlines(random.randrange(0, 10000), )
     print(answer)
     print("hsefsdf")
 
