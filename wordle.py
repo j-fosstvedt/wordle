a7f3c80c8f10e159e9799c66b550917bf8689142
jorgen-tharaldsen-fosstvedt <j.fosstvedt@gmail.com>
2022-05-13 13:17:02 +0200

    has all feautures, haven't really bugtested

--- wordle.py
+++ wordle.py
@@ -1,11 +1,77 @@
 import random
-answersFile = open("C:/Python/wordle/answers.txt", "r")
+from socketserver import ThreadingTCPServer
 
-def startGame():
-    answer = answersFile.readlines(random.randrange(0, 10000), )
+maxWordLength = 5
+maxGuesses = 6
+
+words = []
+
+replay = False
+
+def filterWords():
+    wordsFile = open("C:/Python/wordle/answers.txt", "r")
+    for word in wordsFile:
+        newWord = word.replace("\n", "")
+        if len(newWord) != maxWordLength:
+            continue
+        words.append(newWord)
+
+    wordsFile.close()
+
+def replayCheck():
+    replayCheckString = input("Do you want to play again? (Y/n) ")
+    if replayCheckString == "Y" or replayCheckString == "y" or replayCheckString == "":
+        replay = True
+    else: 
+        replay = False
+    return replay
+
+def playGame():
+    guess = ""
+
+    answer = words[random.randrange(0, len(words))]
+
+    # DEBUG:
     print(answer)
-    print("hsefsdf")
 
-startGame()
+    emoticonString = ""
+    
+    for guesses in range(maxGuesses):
+        guess = input("Guess: ")
+        while len(guess) != 5:
+            print("That's too short or too long, try again!")
+            guess = input("Guess: ")
 
-answersFile.close()
+        while guess not in words:
+            print("That's not in my dictionary! Try again!")
+            guess = input("Guess: ")
+
+        emoticonString = ""
+        correctCount = 0
+
+        i = 0
+        for char in guess:
+            if char == answer[i]:
+                emoticonString += "🟢"
+                correctCount += 1
+            elif char in answer:
+                emoticonString += "🟡"
+            elif char not in answer:
+                emoticonString += "🔴"
+            i += 1
+        print(emoticonString)
+        
+
+        if correctCount == maxWordLength:
+            print("Nice job!")
+            break
+
+    else: print("You failed, the word was " + '"' + answer + '"' + ", but you're too ignorant to understand that. ")
+
+
+filterWords()
+
+while True:
+    playGame()
+    if replayCheck() == False:
+        break
